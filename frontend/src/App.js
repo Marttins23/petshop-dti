2ecd339bc3d1d1cab8fed657c3c2c9ad44b92d3a
Marttins23 <mateus_martins18@yahoo.com.br>
2024-04-24 13:44:04 -0300

    Adiciona espacamento superior ao conteudo da aplicacao.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import FormularioPetshop from './components/FormularioPetshop';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App mt-5">
       <div className="container-xl text-center min-vh-100">
         <h2 className="text-muted">Encontre o Melhor Petshop</h2>
-        <p className="text-secondary my-3 fs-6 fw-semibold">
+        <p className="text-secondary my-3 fs-6 fw-semibold pb-4">
           Descubra qual é a melhor opção (mais barata) de Petshop para levar os seus amiguinhos de 4 patas.
         </p>
         <FormularioPetshop />
